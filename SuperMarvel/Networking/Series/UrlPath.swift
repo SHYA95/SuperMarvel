8860b2c99058f98f6db3837e57b20b420255329e
SHYA95 <yassershrouk254@gmail.com>
2023-11-22 10:42:39 +0200

    add moya layer

---
+++ SuperMarvel/Networking/Series/UrlPath.swift
@@ -0,0 +1,13 @@
+//
+//  UrlPath.swift
+//  SuperMarvel
+//
+//  Created by Shrouk Yasser on 17/11/2023.
+//
+
+enum UrlPath: String {
+    
+    // MARK: - Series
+    case characters = "/series"
+    
+}
